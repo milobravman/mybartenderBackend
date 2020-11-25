58ee15f2551b6b995c6d72c513c739ff088fea11
github username <github email address>
2020-11-25 07:43:38 -0500

    backend done

--- myapp/app/controllers/drink_groups_controller.rb
+++ myapp/app/controllers/drink_groups_controller.rb
@@ -1,11 +1,23 @@
 class DrinkGroupsController < ApplicationController
 
+
+    skip_before_action :verify_authenticity_token, :only => [:create]
     
     def index
 
         tab = DrinkGroup.all
         render json: tab.to_json()
 
+    end
+
+    def create 
+        drink = Drink.find(params[:drink_id])
+        group = Group.find(params[:group_id])
+
+        tab = DrinkGroup.create(
+            drink: drink,
+            group: group,
+        )
     end
 
 end
