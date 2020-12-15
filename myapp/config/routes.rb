9a3f7ef35d0ed3361b3f94c0f5d4b22af8baa5c8
Milo Bravman <Milobravman20@gmail.com>
2020-12-15 12:59:17 -0500

    routes file comments

--- myapp/config/routes.rb
+++ myapp/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #full CRUD routes for all of these modles several of these routes are not used and are secrutiy vunerabilties at some point I should add the approprate [:only] modifires
+
   resources :tables
   resources :groups
   resources :drinks
@@ -7,6 +10,7 @@ Rails.application.routes.draw do
   resources :food_groups
   resources :foods
 
+ # Unique routes to PATCH FoodGroup and DrinkGroup modles 
 
   patch "/food_groups/:id/c", to: "food_groups#change_status", as: "change_status"
   patch "/drink_groups/:id/c", to: "drink_groups#change_drink_status", as: "change_drink_status" 
