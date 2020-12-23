9a21a00dd20b916e4f0672dedf6ff8b88bc725d2
Milo Bravman <Milobravman20@gmail.com>
2020-12-23 17:15:27 -0500

    Comment on groups Controller

--- myapp/app/models/group.rb
+++ myapp/app/models/group.rb
@@ -1,9 +1,14 @@
 class Group < ApplicationRecord
+    # belongs_to :table, optional: true To open the door to have a record of all the groups.
+    # This way when I PATCH the tableID to nil it should not break the model 
     belongs_to :table, optional: true
 
+    #Has many drinks_groups and it has many drinks (Through drink groups). The Drink_group represents one joining of a group_id and a drink_id while a drink is a menu object.
     has_many :drink_groups, dependent: :destroy
     has_many :drinks, through: :drink_groups
 
+    # Same idea with Food here if Sam's group has a pizza that is recoured as a food_group. 
+    # It is possible that the wat these tables are set up the has_many :foods is not nessisary. 
     has_many :food_groups, dependent: :destroy
     has_many :foods, through: :food_groups
 end
